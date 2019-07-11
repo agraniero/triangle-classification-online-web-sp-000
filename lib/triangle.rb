a9509afa4e7be0477d723526180a0ac4deb17350
Anthony Graniero <ananfro@gmail.com>
2019-07-11 03:39:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,11 @@
 class Triangle
   
-  def initialize()
-    
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
   
 end
