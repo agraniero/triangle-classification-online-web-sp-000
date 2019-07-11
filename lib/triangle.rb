77907e58114a39b35c9928374817840289c951b7
Anthony Graniero <ananfro@gmail.com>
2019-07-11 05:23:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
   
   def kind 
-    proper_t
+    legit_triangle
     if side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side1 = side3
